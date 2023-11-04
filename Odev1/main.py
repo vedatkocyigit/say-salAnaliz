0fdd2f143bc0257001dfced4aa82338249a01a00
vedatkocyigit <116923628+vedatkocyigit@users.noreply.github.com>
2023-11-04 14:10:02 +0300

    sayısalAnalizOdev1

---
+++ Odev1/main.py
@@ -0,0 +1,20 @@
+import math;
+
+#1.terim için
+
+gercekDeger=math.cos(math.pi/5);
+bulunanDeger=1;
+kesmeHatasi=gercekDeger - bulunanDeger;
+
+print(kesmeHatasi);
+
+
+#2.terim için
+
+gercekDeger2=math.cos(math.pi/5);
+bulunanDeger2=(1+(-1/2*0.39478417604));
+kesmeHatasi2=gercekDeger2 - bulunanDeger2;
+
+print(kesmeHatasi2);
+
+
